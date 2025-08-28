3e52d704f8de778e16177aecd80a30896ff00377
kwalkerbcm <kw5@bcm.edu>
2025-08-28 14:15:35 -0500

    Update ttest_df_gsea_analysis.R

--- Rscripts/ttest_df_gsea_analysis.R
+++ Rscripts/ttest_df_gsea_analysis.R
@@ -1,6 +1,7 @@
 library(OlinkAnalyze)
 library(dplyr)
-
+library(arrow)
+setwd('Desktop/HACKATHON/')
 # read in files
 npx_parquet <- read_parquet("Olink_datafile_1.parquet")
 manifest_file <- read.csv("SexManifest_1.csv", header = TRUE, sep = ",")
@@ -8,11 +9,11 @@ manifest_file <- read.csv("SexManifest_1.csv", header = TRUE, sep = ",")
 npx_with_manifest <- merge(npx_parquet, manifest_file[, c("SampleID", "Gender")], by = "SampleID", all.x = TRUE)
 # remove controls
 npx_df <- npx_with_manifest |>
-     dplyr::filter(!grepl("control", SampleID, ignore.case = TRUE))
+     dplyr::filter(!grepl("control", SampleType, ignore.case = TRUE))
 ttest_results <- olink_ttest(
      df = npx_df,
      variable = "Gender",
      alternative = "two.sided")
 # pathway analysis
-gsea_results <- olink_pathway_enrichment(data = npx_data1, test_results = ttest_results)
+gsea_results <- olink_pathway_enrichment(data = npx_df, test_results = ttest_results)
 
