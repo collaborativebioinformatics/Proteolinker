1bb2e93da0c6a76fe93a33ed6fc8ea6fe9559e13
qiaoyanw <qiaoyanw@bcm.edu>
2025-08-28 14:52:32 -0500

    Update ttest_df_gsea_analysis.R

--- Rscripts/ttest_df_gsea_analysis.R
+++ Rscripts/ttest_df_gsea_analysis.R
@@ -3,17 +3,53 @@ library(dplyr)
 library(arrow)
 setwd('Desktop/HACKATHON/')
 # read in files
-npx_parquet <- read_parquet("Olink_datafile_1.parquet")
-manifest_file <- read.csv("SexManifest_1.csv", header = TRUE, sep = ",")
-# merge Gender info to npx dataframe
-npx_with_manifest <- merge(npx_parquet, manifest_file[, c("SampleID", "Gender")], by = "SampleID", all.x = TRUE)
-# remove controls
-npx_df <- npx_with_manifest |>
-     dplyr::filter(!grepl("control", SampleType, ignore.case = TRUE))
+Olink_datafile <- read_parquet("Olink_datafile_1.parquet")
+Gender_datafile <- read.csv("SexManifest_1.csv", header = TRUE, sep = ",")
+npx_parquet<-Olink_datafile |>
+  filter(!grepl("CONTROL", SampleType))|>
+  filter(!grepl("ctrl", AssayType))|>
+  filter(!is.na(NPX))|>
+  left_join(Gender_datafile, by=c("SampleID"))
+                
 ttest_results <- olink_ttest(
-     df = npx_df,
-     variable = "Gender",
-     alternative = "two.sided")
-# pathway analysis
-gsea_results <- olink_pathway_enrichment(data = npx_df, test_results = ttest_results)
+    df = npx_parquet,
+    variable = "Gender",
+    alternative = "two.sided")
 
+Results<-as.data.frame(ttest_results)
+
+# volcano plots
+pdf(file = "/Users/qiaoyanw/Desktop/ttest_plot.pdf",
+    width = 11,
+    height = 8)
+Results |>
+  ggplot(aes(x = estimate, y = -log10(Adjusted_pval), col = Threshold)) +
+  geom_point() +
+  set_plot_theme()
+dev.off()
+
+# pathway analysis
+
+ontology_opt <- readline("Enter your ontology options (1 for KEGG, 2 for GO, 3 for Reactome, 4 for all): ")
+if (ontology_opt==1){
+  gsea_results <- olink_pathway_enrichment(data = npx_parquet, test_results = ttest_results, ontology = "KEGG")
+}
+if (ontology_opt==2){
+  gsea_results <- olink_pathway_enrichment(data = npx_parquet, test_results = ttest_results, ontology = "GO")
+}
+if (ontology_opt==3){
+  gsea_results <- olink_pathway_enrichment(data = npx_parquet, test_results = ttest_results, ontology = "Reactome")
+}
+
+if (ontology_opt==4){
+  gsea_results <- olink_pathway_enrichment(data = npx_parquet, test_results = ttest_results)
+}
+
+pdf(file = "/Users/qiaoyanw/Desktop/Pathway_Plot.pdf",
+    width = 11,
+    height = 8)
+# GSEA Heatmap from t-test results
+olink_pathway_heatmap(enrich_results = gsea_results, test_results = ttest_results)
+# GSEA bar chart from t-test results 
+olink_pathway_visualization(enrich_results = gsea_results, method = "GSEA", number_of_terms = 15)
+dev.off()
