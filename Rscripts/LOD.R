aefaf19e14e8609f68241b0868e2544c9cfac9b1
qiaoyanw <qiaoyanw@bcm.edu>
2025-08-28 13:51:09 -0500

    Update LOD.R

--- Rscripts/LOD.R
+++ Rscripts/LOD.R
@@ -23,18 +23,32 @@ npx_parquet<-Olink_datafile|>
 if (LOD_opt==1){
   fixedLOD_filepath <- "/Users/qiaoyanw/Desktop/ExploreHT_Fixed LOD_2024-12-19.csv"
   LOD_npx_parquet <-olink_lod(npx_parquet, lod_file_path = fixedLOD_filepath, lod_method = "FixedLOD")
-  }
-LOD_model<-LOD_npx_parquet|>
-  dplyr::mutate(Threshold  = ifelse(NPX<LOD,"LOD","Non_LOD"))|>
-  mutate(Threshold = replace_na(Threshold, "Above_LOD"))
+  
+  LOD_model<-LOD_npx_parquet|>
+    dplyr::mutate(Threshold  = ifelse(NPX<LOD,"LOD","Above_LOD"))|>
+    mutate(Threshold = replace_na(Threshold, "Above_LOD"))
+}
 
 if (LOD_opt==2){
   LOD_Assay <- npx_parquet %>% 
     filter(grepl('NEGATIVE_CONTROL',SampleType, ignore.case = TRUE)) %>%
-    group_by(PlateID, Assay) %>%
-    summarise(LOD=mean(NPX)+3*sd(NPX),PCNormalizedLOD=mean(PCNormalizedNPX)+3*sd(PCNormalizedNPX))
-  LOD_npx_parquet_NCStdev<-select(LOD_Assay, PlateID, Assay, PCNormalizedLOD, LOD) %>%
-    left_join(npx_parquet, by=c("Assay", "PlateID"))
-}
-LOD_model_NCStdev<-LOD_npx_parquet_NCStdev|>
-  dplyr::mutate(Threshold  = ifelse(NPX<LOD,"LOD","Above_LOD"))
+    group_by(Assay) %>%
+    summarise(LOD=median(NPX)+3*sd(NPX),PCNormalizedLOD=median(PCNormalizedNPX)+3*sd(PCNormalizedNPX))
+  LOD_npx_parquet_NCStdev<-select(LOD_Assay, Assay, PCNormalizedLOD, LOD) %>%
+    left_join(npx_parquet, by=c("Assay"))
+  
+  LOD_model<-LOD_npx_parquet_NCStdev|>
+    dplyr::mutate(Threshold  = ifelse(NPX<LOD,"LOD","Above_LOD"))
+  }
+  
+  # volcano plots, two plates combined
+  pdf(file = "/Users/qiaoyanw/Desktop/LOD_VolcanoPlot.pdf",
+      width = 11,
+      height = 8)
+  LOD_model |>
+    filter(grepl('SC',SampleID, ignore.case = TRUE))|>
+    ggplot(aes(x = (NPX-LOD), y = NPX, col = Threshold)) +
+    geom_point() +
+    set_plot_theme() +
+    facet_wrap(.~SampleID)
+  dev.off()
